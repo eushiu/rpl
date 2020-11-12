fd0977ddf4b09037cfa102d497fe16babb9a3eb9
Eugene Shiu <shiueugene@gmail.com>
2020-11-12 11:56:43 -0500

    update

--- js/script2.js
+++ js/script2.js
@@ -7,14 +7,21 @@ function computerPlay(){
 
   function playRound(playerSelection, computerSelection) {
     //playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    const para = document.createElement("p");
+    const result = document.createElement("p");
+    para.textContent = `You chose ${playerSelection}, Computer chose: ${computerSelection}`;
+    scoreboard.appendChild(para);
+    
     if (playerSelection == "Rock"){
       if (computerSelection == "Rock") {
         return "Tie Game...Rock"
         }
       else if (computerSelection == "Paper") {
+        computerScore++;
         return "You Lose! Paper beats Rock"
         }
       else {
+      playerScore++;
       return "You Win! Rock beats Scissors"
       }
     }
@@ -23,9 +30,11 @@ function computerPlay(){
         return "Tie Game...Scissors"
         }
       else if (computerSelection == "Rock") {
+        computerScore++;
         return "You Lose! Rock beats Scissors"
         }
       else {
+      playerScore++;
       return "You Win! Scissors beat Paper"
       }
     }
@@ -34,25 +43,29 @@ function computerPlay(){
         return "Tie Game...Paper"
         }
       else if (computerSelection == "Scissors") {
+        computerScore++;
         return "You Lose! Scissors beat Paper"
         }
       else {
+      playerScore++;
       return "You Win! Paper beat Rock"
       }
     }
   }
 
   let playerChoice;
+  let playerScore = 0;
+  let computerScore = 0;
 
-  function playerSelect() {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-      button.addEventListener('click', (e) => {
-      playerChoice = (button.id);
-      console.log(button.id)
-      console.log(playRound(playerChoice, computerPlay()));
-      });
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+    playerChoice = (button.id);
+    playRound(playerChoice, computerPlay());
     });
-  };
+  });
 
-  playerSelect()
+const container = document.querySelector(".container");
+const scoreboard = document.createElement("div");
+scoreboard.textContent = `Player score: ${playerScore}; Computer score: ${computerScore}`;
+container.appendChild(scoreboard);
